2d294a154fcccd39845ff482becb357fd983474c
Michael McCourt <mkmccjr@gmail.com>
2026-06-10 09:37:26 -0700

    add migration test

--- test/sturdy/sqlite/migrate_test.clj
+++ test/sturdy/sqlite/migrate_test.clj
@@ -3,8 +3,8 @@
    [clojure.test :refer [deftest testing is]]
    [sturdy.sqlite.migrate :as migrate]
    [sturdy.sqlite.test :as db-test]
-   [sturdy.sqlite.types :as types]
-   [next.jdbc :as jdbc]))
+   [next.jdbc :as jdbc]
+   [ragtime.repl :as rr]))
 
 (set! *warn-on-reflection* true)
 
@@ -14,17 +14,28 @@
   (testing "migrate! and rollback! call ragtime with correct config"
     (let [ragtime-migrate-called (atom false)
           ragtime-rollback-called (atom false)]
-      (with-redefs [ragtime.repl/migrate  (fn [cfg]
-                                            (reset! ragtime-migrate-called true)
-                                            (is (= "testdb" (get-in cfg [:reporter-data :db-name] "testdb")))
-                                            ;; test the reporter function by calling it
-                                            (let [reporter (:reporter cfg)]
-                                              (reporter nil :up "001-test")
-                                              (reporter nil :down "001-test")))
-                    ragtime.repl/rollback (fn [cfg]
-                                            (reset! ragtime-rollback-called true))]
+      (with-redefs [rr/migrate  (fn [cfg]
+                                  (reset! ragtime-migrate-called true)
+                                  (is (= "testdb" (get-in cfg [:reporter-data :db-name] "testdb")))
+                                  ;; test the reporter function by calling it
+                                  (let [reporter (:reporter cfg)]
+                                    (reporter nil :up "001-test")
+                                    (reporter nil :down "001-test")))
+                    rr/rollback (fn [_cfg]
+                                  (reset! ragtime-rollback-called true))]
         (migrate/migrate! "testdb" {} "test-migrations")
         (is @ragtime-migrate-called)
 
         (migrate/rollback! "testdb" {} "test-migrations")
-        (is @ragtime-rollback-called)))))
+        (is @ragtime-rollback-called))))
+
+  (testing "Real migration execution on test DB"
+    (db-test/with-test-db [{:keys [datasource]} "test-migrations-db" {:classpath-prefix "test-migrations"}]
+      ;; with-test-db automatically runs migrate! using the classpath-prefix, so the table should exist
+      (let [res (jdbc/execute! datasource ["SELECT name FROM sqlite_master WHERE type='table' AND name='test_table'"])]
+        (is (= 1 (count res)) "test_table should have been created"))
+
+      ;; Now roll it back
+      (migrate/rollback! "test-migrations-db" datasource "test-migrations")
+      (let [res (jdbc/execute! datasource ["SELECT name FROM sqlite_master WHERE type='table' AND name='test_table'"])]
+        (is (= 0 (count res)) "test_table should have been dropped")))))
